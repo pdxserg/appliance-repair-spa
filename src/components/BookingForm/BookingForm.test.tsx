3119c77bd1be1a03c55035d0a116d7192a0666c8
SergeyStarchenko <pdxserg@gmail.com>
2025-05-03 15:35:06 -0700

    feat: unit test

---
+++ src/components/BookingForm/BookingForm.test.tsx
@@ -0,0 +1,11 @@
+
+import {describe, it, expect} from "vitest";
+
+describe("math",()=>{
+	it('add numbers correctly', () => {
+		expect(2+1).toBe(3)
+		expect(2+3).toBe(5)
+		expect(2+2).toBe(4)
+
+	});
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,42 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+
+
+ import react from '@vitejs/plugin-react'
+import {resolve} from 'path';
+import {defineConfig} from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    minify: 'terser',
-    cssCodeSplit: true,
-  },
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.ts',
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+		},
+	},
+	css: {
+		modules: {
+			localsConvention: 'camelCase',
+		},
+	},
+	server: {
+		port: 3000,
+	},
+	build: {
+		minify: 'terser',
+		cssCodeSplit: true,
+	},
 });
+
+
+//
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: './src/test/setup.ts',
+//   },
+// });
